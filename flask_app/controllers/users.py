fbf7d122cf892c676e3de83f34f3837065a3e987
Andrewgl22 <ledermanmusic@gmail.com>
2023-10-05 22:34:54 -0700

    final commit

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -64,64 +64,6 @@ def dashboard():
     user = User.get_one_by_id({"id":session['user_id']})
     return render_template('index.html', books=books, user=user)
 
-@app.route('/user/new')
-def user_form():
-    return render_template('form.html')
-
-@app.route('/user', methods=['POST'])
-def create_user():
-    print('this is the form data:', request.form)
-
-    if not User.validate_user(request.form):
-        return redirect('/user/new')
-
-    data = {
-        "username":request.form['username'],
-        "email":request.form['email'],
-        "age":request.form['age'],
-        "country":request.form['country']
-    }
-    User.save(data)
-    return redirect('/html')
-
-@app.route('/user/<int:id>')
-def one_user(id):
-    data = {
-        "id":id
-    }
-    user = User.get_one_by_id(data)
-    return render_template('one_user.html',user=user)
-
-# sends us to the edit form with one user's information
-@app.route('/user/<int:id>/edit')
-def edit_form(id):
-    data = {
-        "id":id
-    }
-    user = User.get_one_by_id(data)
-    return render_template('update_form.html',user=user)
-
-# update a user
-@app.route('/user/<int:id>', methods=['POST'])
-def update(id):
-    data = {
-        "id":id,
-        "username":request.form['username'],
-        "email":request.form['email'],
-        "age":request.form['age'],
-        "country":request.form['country']
-    }
-    User.update(data)
-    return redirect('/html')
-
-@app.route('/user/<int:id>/delete')
-def delete(id):
-    data = {
-        "id":id
-    }
-    User.delete(data)
-    return redirect('/html')
-
 @app.route('/user_list')
 def user_list():
     users = User.get_users_w_books()
